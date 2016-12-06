df9dcf90e6160c83b6196e2574c7d4c342787acd
KSP-TaxiService <chinglihlim@gmail.com>
2016-12-06 23:08:31 +0800

    Overlook the new files

---
+++ src/CommNetConstellation/CommNetLayer/CNCCommNetNetwork.cs
@@ -0,0 +1,33 @@
+﻿using CommNet;
+
+namespace CommNetConstellation.CommNetLayer
+{
+    public class CNCCommNetNetwork : CommNetNetwork
+    {
+        public static void upgradeToCNCCommNetNetwork()
+        {
+            CNCLog.Debug("CNCCommNetNetwork.upgradeToCNCCommNetNetwork()");
+            CommNetNetwork.Instance = new CNCCommNetNetwork();
+        }
+
+        public CNCCommNetNetwork() : base()
+        {
+            base.CommNet = new CNCCommNetwork();
+        }
+
+        public override CommNetwork CommNet
+        {
+            get
+            {
+                CNCLog.Debug("CNCCommNetNetwork.CommNet - get");
+                return base.CommNet;
+            }
+
+            set
+            {
+                CNCLog.Debug("CNCCommNetNetwork.CommNet - set");
+                base.CommNet = value;
+            }
+        }
+    }
+}
---
+++ src/CommNetConstellation/CommNetLayer/CNCCommNode.cs
@@ -0,0 +1,42 @@
+﻿using CommNet;
+
+namespace CommNetConstellation.CommNetLayer
+{
+    public class CNCCommNode : CommNode
+    {
+        public CNCCommNode()
+        {
+
+        }
+
+        //Copy constructor
+        public CNCCommNode(CommNode stockCommNode)
+        {
+            this.antennaRelay = stockCommNode.antennaRelay;
+            this.antennaTransmit = stockCommNode.antennaTransmit;
+            this.distanceOffset = stockCommNode.distanceOffset;
+            this.isControlSource = stockCommNode.isControlSource;
+            this.isControlSourceMultiHop = stockCommNode.isControlSourceMultiHop;
+            this.isHome = stockCommNode.isHome;
+            this.OnLinkCreateSignalModifier = stockCommNode.OnLinkCreateSignalModifier;
+            this.OnNetworkPostUpdate = stockCommNode.OnNetworkPostUpdate;
+            this.OnNetworkPreUpdate = stockCommNode.OnNetworkPreUpdate;
+            this.scienceCurve = stockCommNode.scienceCurve;
+            this._name = stockCommNode.name;
+            this._position = stockCommNode.position;
+        }
+        
+        public override void NetworkPreUpdate()
+        {
+            CNCLog.Debug("CNCCommNode.NetworkPreUpdate()");
+            base.NetworkPreUpdate();
+        }
+
+        public override void NetworkPostUpdate()
+        {
+            CNCLog.Debug("CNCCommNode.NetworkPostUpdate()");
+            base.NetworkPostUpdate();
+        }
+        
+    }
+}
